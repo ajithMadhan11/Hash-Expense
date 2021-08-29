6d21d1f896f098970d9a3aae8dc18069818e89c2
Ajith madhan <ajithmadhan.dev113@gmail.com>
2021-08-29 23:00:50 +0530

    updated config file

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -4,14 +4,7 @@ import "firebase/auth";
 import "firebase/database";
 
 const app = {
-  apiKey: "AIzaSyDkA7_68QcrXxgdI84lh6tkOOZfsSULVZE",
-  authDomain: "expense-tracker-c0145.firebaseapp.com",
-  databaseURL: "https://expense-tracker-c0145-default-rtdb.firebaseio.com",
-  projectId: "expense-tracker-c0145",
-  storageBucket: "expense-tracker-c0145.appspot.com",
-  messagingSenderId: "779290897206",
-  appId: "1:779290897206:web:6fcb564352584fba9fe197",
-  measurementId: "G-5Z45WE5300",
+  //your congig
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(app);
